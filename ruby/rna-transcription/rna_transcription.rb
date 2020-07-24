1d3928b71131bec253c807960fc116b4dfe4405d
chook <dylanw212@gmail.com>
2020-07-24 11:41:48 +1000

    rework RNA transcription

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,28 +1,7 @@
-=begin
-Write your code for the 'Rna Transcription' exercise in this file. Make the tests in
-`rna_transcription_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/rna-transcription` directory.
-=end
-
 class Complement
-  def self.of_dna(string)
-    string.split('').map { |letter| converter(letter) }.join('')
-  end
+  CONVERTER = { 'C': 'G', 'G': 'C', 'T': 'A', 'A': 'U' }.freeze
 
-  def self.converter(letter)
-    case letter
-    when 'C'
-      'G'
-    when 'G'
-      'C'
-    when 'T'
-      'A'
-    when 'A'
-      'U'
-    else
-      ''
-    end
+  def self.of_dna(string)
+    string.chars.map { |letter| CONVERTER[letter.to_sym] }.join('')
   end
 end
