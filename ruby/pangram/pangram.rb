5b3747cd80146681850d28206d1dec7850f90a7a
chook <dylanw212@gmail.com>
2020-07-26 13:53:03 +1000

    several pangram tests passing

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,8 +1,5 @@
-=begin
-Write your code for the 'Pangram' exercise in this file. Make the tests in
-`pangram_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/pangram` directory.
-=end
-
+class Pangram
+  def self.pangram?(string)
+    ('a'..'z').to_a.join == string.delete(' ').chars.sort.uniq.join.downcase
+  end
+end
