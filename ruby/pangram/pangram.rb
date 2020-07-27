eba0bf50616e93073d5458db96eb7c48ec2bc8a1
chook <dylanw212@gmail.com>
2020-07-27 10:51:47 +1000

    add solution to pangram

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,5 +1,5 @@
 class Pangram
   def self.pangram?(string)
-    ('a'..'z').to_a.join == string.delete(' ').chars.sort.uniq.join.downcase
+    ('a'..'z').to_a.join == string.delete(' _0-9"\'').chars.sort.uniq.join.downcase
   end
 end
