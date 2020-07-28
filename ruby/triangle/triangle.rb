8d2d8d31e468c4ac74066cbcd7372733e6b2e234
chook <dylanw212@gmail.com>
2020-07-28 13:51:32 +1000

    first triangle solution

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -7,11 +7,32 @@ class Triangle
   end
 
   def equilateral?
-    sides.any?(0) ? false : sides.all?(sides.first)
+    valid_triangle? ? sides.all?(sides.first) : false
   end
 
   def isosceles?
-
+    valid_triangle? ? sides.uniq.length <= 2 : false
   end
 
+  def scalene?
+    valid_triangle? ? sides.uniq.length == 3 : false
+  end
+
+  private
+
+  def valid_triangle?
+    if sides.any?(0) || triangle_inequality?
+      false
+    else
+      true
+    end
+  end
+
+  def triangle_inequality?
+    if sides[0] > sides[1] + sides[2] || sides[1] > sides[0] + sides[2] || sides[2] > sides[0] + sides[1]
+      true
+    else
+      false
+    end
+  end
 end
