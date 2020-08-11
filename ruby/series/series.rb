6b1e31e83a752991640e74a6b626c8b8ecf7d037
chook <dylanw212@gmail.com>
2020-08-11 23:13:39 +1000

    refactor series

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -8,11 +8,6 @@ class Series
   def slices(slice_len)
     raise ArgumentError if slice_len > num_string.length
 
-    res = num_string.chars.each_with_index.map do |_elt, idx|
-      next unless num_string[idx...idx + slice_len].length == slice_len
-
-      num_string[idx...idx + slice_len]
-    end
-    res.compact
+    num_string.chars.each_cons(slice_len).map(&:join)
   end
 end
