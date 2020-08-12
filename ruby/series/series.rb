bce5dd7eaf2b0982984da551499db14c9f630c0f
chook <dylanw212@gmail.com>
2020-08-12 22:13:46 +1000

    implement feedback

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,13 +1,11 @@
 class Series
-  attr_reader :num_string
-
   def initialize(num_string)
     @num_string = num_string
   end
 
   def slices(slice_len)
-    raise ArgumentError if slice_len > num_string.length
+    raise ArgumentError unless slice_len <= @num_string.size
 
-    num_string.chars.each_cons(slice_len).map(&:join)
+    @num_string.each_char.each_cons(slice_len).map(&:join)
   end
 end
