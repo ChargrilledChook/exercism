424bb020a6ddae8c869e35ae1b66a70f125e8792
Dylan Williams <happyfrog578+github@gmail.com>
2020-08-16 18:13:49 +1000

    add mentor feedback to word count

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,9 +1,11 @@
 class Phrase
+  PATTERN = /\b[\w']+\b/.freeze
+
   def initialize(words)
     @words = words
   end
 
   def word_count
-    @words.downcase.scan(/\b[\w']+\b/).each_with_object(Hash.new(0)) { |word, hash| hash[word] += 1 }
+    @words.downcase.scan(PATTERN).each_with_object(Hash.new(0)) { |word, hash| hash[word] += 1 }
   end
 end
