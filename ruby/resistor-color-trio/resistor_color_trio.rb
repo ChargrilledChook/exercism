a1c664e8ccd9bfeaeeed7ce3d8003f29d8e5b3a2
Dylan Williams <happyfrog578+github@gmail.com>
2020-08-16 15:59:15 +1000

    resistor color trio complete

--- ruby/resistor-color-trio/resistor_color_trio.rb
+++ ruby/resistor-color-trio/resistor_color_trio.rb
@@ -1,9 +1,33 @@
 class  ResistorColorTrio
-  def initialize(colors)
+  CODES = %w[black brown red orange yellow green blue violet grey white].freeze
 
+  def initialize(colors)
+    @colors = check_colors(colors)
   end
 
   def label
-    "Resistor value: 33 ohms"
+    "Resistor value: #{convert_code}"
+  end
+
+  private
+
+  def convert_code
+    code = CODES.index(@colors[0]).to_s + CODES.index(@colors[1]).to_s
+    code << ('0' * CODES.index(@colors[2]))
+    check_kiloohms(code)
+  end
+
+  def check_kiloohms(code)
+    if code.to_i >= 1000
+      code.delete_suffix('000') + ' kiloohms'
+    else
+      code + ' ohms'
+    end
+  end
+
+  def check_colors(colors)
+    raise ArgumentError unless colors.all? { |color| CODES.include?(color) }
+
+    colors
   end
 end
--- ruby/resistor-color-trio/resistor_color_trio_test.rb
+++ ruby/resistor-color-trio/resistor_color_trio_test.rb
@@ -14,22 +14,22 @@ class ResistorColorTrioTest < Minitest::Test
   end
 
   def test_red_and_black_and_red
-    skip
+    # skip
     assert_equal "Resistor value: 2 kiloohms", ResistorColorTrio.new(["red", "black", "red"]).label
   end
 
   def test_green_and_brown_and_orange
-    skip
+    # skip
     assert_equal "Resistor value: 51 kiloohms", ResistorColorTrio.new(["green", "brown", "orange"]).label
   end
 
   def test_yellow_and_violet_and_yellow
-    skip
+    # skip
     assert_equal "Resistor value: 470 kiloohms", ResistorColorTrio.new(["yellow", "violet", "yellow"]).label
   end
 
   def test_invalid_color
-    skip
+    # skip
     assert_raises(ArgumentError) do
       ResistorColorTrio.new(["yellow", "purple", "black"]).label
     end
