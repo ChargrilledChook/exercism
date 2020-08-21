1c047984c3d15ed025550373eb7d93e22729b985
chook <dylanw212@gmail.com>
2020-08-21 10:25:04 +1000

    add mentor feedback

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,6 +2,6 @@ class Hamming
   def self.compute(dna1, dna2)
     raise ArgumentError unless dna1.size == dna2.size
 
-    dna1.each_char.zip(dna2.each_char).reject { |nuc1, nuc2| nuc1 == nuc2 }.count
+    dna1.chars.zip(dna2.chars).count { |nuc1, nuc2| nuc1 != nuc2 }
   end
 end
