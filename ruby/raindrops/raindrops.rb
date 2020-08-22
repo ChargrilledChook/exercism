4a433e39b74f0815a263b0c3578ce3dc0d8718ba
chook <dylanw212@gmail.com>
2020-08-22 16:48:34 +1000

    first iteration complete

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,5 +1,9 @@
-class Raindrops
-  def self.convert(arg)
-    '1'
+module Raindrops
+  def self.convert(num)
+    res = ''
+    res += 'Pling' if (num % 3).zero?
+    res += 'Plang' if (num % 5).zero?
+    res += 'Plong' if (num % 7).zero?
+    res.empty? ? num.to_s : res
   end
 end
