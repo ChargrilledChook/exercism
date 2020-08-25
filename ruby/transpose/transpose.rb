b45f2c1b095c8dae01a833efec0e7e65a03af6fa
Dylan Williams <happyfrog578+github@gmail.com>
2020-08-25 18:08:26 +1000

    add experimental changes

--- ruby/transpose/transpose.rb
+++ ruby/transpose/transpose.rb
@@ -3,3 +3,10 @@ class Transpose
     input.split("\n").map(&:chars).transpose.map(&:join).join("\n")
   end
 end
+
+def test(input)
+  lines = input.split("\n")
+  max_str = lines.max.size
+  lines = lines.map(&:chars).transpose
+  lines.map(&:join).map { |line| line.ljust(max_str, ' ') }.join("\n")
+end
