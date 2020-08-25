ab43c8105b5ec52a23043af7bace917c8d8e0b47
chook <dylanw212@gmail.com>
2020-08-25 16:58:46 +1000

    first test of transpose

--- ruby/transpose/transpose.rb
+++ ruby/transpose/transpose.rb
@@ -1,5 +1,7 @@
 class Transpose
   def self.transpose(input)
+    return '' if input.empty?
 
+    input.chars.join("\n")
   end
 end
--- ruby/transpose/transpose_test.rb
+++ ruby/transpose/transpose_test.rb
@@ -22,7 +22,7 @@ class TransposeTest < Minitest::Test
   end
 
   def test_two_characters_in_a_column
-    skip
+    # skip
     input = "A\n1"
 
     expected = "A1"
