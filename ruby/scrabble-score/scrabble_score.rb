f478f8ec86eb9db9f99ff78140d9bb9cddf73ef6
chook <dylanw212@gmail.com>
2020-09-23 12:54:47 +1000

    iteration 1 scrabble complete

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,8 +1,52 @@
-=begin
-Write your code for the 'Scrabble Score' exercise in this file. Make the tests in
-`scrabble_score_test.rb` pass.
+class Scrabble
+  LETTER_SCORES = {
+    'a' => 1,
+    'b' => 3,
+    'c' => 3,
+    'd' => 2,
+    'e' => 1,
+    'f' => 4,
+    'g' => 2,
+    'h' => 4,
+    'i' => 1,
+    'j' => 6,
+    'k' => 5,
+    'l' => 1,
+    'm' => 3,
+    'n' => 1,
+    'o' => 1,
+    'p' => 3,
+    'q' => 10,
+    'r' => 1,
+    's' => 1,
+    't' => 1,
+    'u' => 1,
+    'v' => 4,
+    'w' => 4,
+    'x' => 8,
+    'y' => 4,
+    'z' => 10
+  }.freeze
 
-To get started with TDD, see the `README.md` file in your
-`ruby/scrabble-score` directory.
-=end
+  def self.score(input)
+    input.each_char.reduce(0) { |res, char| res + LETTER_SCORES.fetch(char, 0) }
+  end
 
+  def initialize(input)
+    @input = clean_input(input)
+  end
+
+  def score
+    input.each_char.reduce(0) { |res, char| res + LETTER_SCORES.fetch(char, 0) }
+  end
+
+  private
+
+  attr_reader :input
+
+  def clean_input(input)
+    return '' unless input
+
+    input.downcase
+  end
+end
