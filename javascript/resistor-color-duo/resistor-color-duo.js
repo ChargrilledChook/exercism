0506916817269e70680634e5d079589cc7b0036e
chook <dylanw212@gmail.com>
2020-11-11 09:30:58 +1100

    dry out legend object

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,16 +1,16 @@
 export const decodedValue = (colours) => {
- const legend = {
-   black: '0',
-   brown: '1',
-   red: '2',
-   orange: '3',
-   yellow: '4',
-   green: '5',
-   blue: '6',
-   violet: '7',
-   grey: '8',
-   white: '9'
- }
+  return Number(legend[colours[0]] + legend[colours[1]]);
+};
 
-return Number(legend[colours[0]] + legend[colours[1]]);
+export const legend = {
+  black: '0',
+  brown: '1',
+  red: '2',
+  orange: '3',
+  yellow: '4',
+  green: '5',
+  blue: '6',
+  violet: '7',
+  grey: '8',
+  white: '9'
 };
