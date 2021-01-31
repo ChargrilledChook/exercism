491d4b45754333840fcc0cc37c233c0e671654c5
chook <dylanw212@gmail.com>
2021-02-01 10:51:14 +1100

    first iteration of space age passing tests

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,3 +1,16 @@
-export const age = () => {
-  return 31.69;
+export const planetRatios = {
+  earth: 1.0,
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
+
+export const earthYear = 31_557_600;
+
+export const age = function ageOnPlanet(planet, time) {
+  return parseFloat((time / earthYear / planetRatios[planet]).toFixed(2));
 };
